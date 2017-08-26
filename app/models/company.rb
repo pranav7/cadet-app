b3a4deb9c2159bd2b4aee0b5b8e96b5290306e1d
Pranav Singh <pranav@supportbee.com>
2017-08-26 20:07:35 +0530

    Adds validations and associations to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+  has_many :users
+
+  validates :subdomain, uniqueness: true, presence: true
+  validates :name, presence: true
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { should have_many :users }
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:subdomain) }
+    it { should validate_uniqueness_of(:subdomain) }
+
+    it { should validate_presence_of(:name) }
+  end
 end

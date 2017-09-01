592fd4f8f21bcd66ae857c249fa0e6d0628fef12
Pranav Singh <pranav@supportbee.com>
2017-09-01 12:56:20 +0530

    Updates devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  devise_for :users
+  devise_for :users, path: '',
+    path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+      passwords: 'users/passwords',
+      invitations: 'users/invitations'
+  }
+
   resources :posts, only: [:new, :create, :index, :show] do
     member do
       resources :comments, only: [:create]
     end
   end
+
+  root "posts#index"
 end

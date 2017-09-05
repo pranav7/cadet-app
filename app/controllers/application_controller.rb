69f17d6ef2ef8fddf911bc48ea401f87108ec6d4
Pranav Singh <pranav@supportbee.com>
2017-09-05 18:52:29 +0530

    Ensuring valid subdomain for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :ensure_valid_subdomain!
+
   helper_method :current_company
 
   def current_company
@@ -7,6 +10,14 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def ensure_valid_subdomain!
+    return unless user_signed_in?
+    return if current_company
+    return if current_user.companies.map(&:subdomain).include?(request.subdomains.first)
+
+    redirect_to root_url(host: "#{current_user.companies.first.subdomain}.lvh.me:3000")
+  end
 
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || root_url(host: "#{current_user.companies.first.subdomain}.lvh.me:3000")

db033e50beda60f53d654d67962267786d44831a
Pranav Singh <pranav@supportbee.com>
2017-09-05 17:14:00 +0530

    Setting after sign_in_path for user and sharing session across domains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_company
+
+  def current_company
+    @current_company = Company.find_by_subdomain(request.subdomains.first)
+  end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_url(host: "#{current_user.companies.first.subdomain}.lvh.me:3000")
+  end
 end

33c631585731ec91493cb3014d55679848aadc28
Pranav Singh <pranav@supportbee.com>
2017-09-05 20:18:08 +0530

    Finally! Successfully constrained devise routes to app subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  devise_for :users, path: '',
-    path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' },
-    controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations',
-      passwords: 'users/passwords',
-      invitations: 'users/invitations'
-  }
+  constraints subdomain: 'app' do
+    devise_for :users, path: '',
+      path_names: { sign_in: 'login', sign_up: 'signup' },
+      controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations',
+        passwords: 'users/passwords',
+        invitations: 'users/invitations'
+    }
+  end
+
+  devise_scope :user do
+    delete "/logout", to: "users/sessions#destroy"
+  end
 
   resources :posts, only: [:new, :create, :index, :show] do
     member do

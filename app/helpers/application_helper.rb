4d1c53615e43a83aae7c6512020e2350a50956c7
Pranav Singh <pranav@supportbee.com>
2017-09-12 00:49:00 +0530

    Adds a helper to render time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_time(created_at)
+    if created_at > 1.week.ago
+      "#{time_ago_in_words(created_at)} ago"
+    else
+      created_at.strftime("%e %B, %Y")
+    end
+  end
 end

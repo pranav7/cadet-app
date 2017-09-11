f169a1c494f79d6e0646f3a666e28d0dae492628
Pranav Singh <pranav@supportbee.com>
2017-09-11 18:11:45 +0530

    Removing binding

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -18,7 +18,6 @@ RSpec.describe CommentsController, type: :controller do
 
     it "redirects to back to where it came from" do
       post :create, params: { post_id: _post.id, comment: comment_params }
-      binding.pry
       expect(response).to redirect_to(back)
     end
 

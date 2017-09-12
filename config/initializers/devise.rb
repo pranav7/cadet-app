c4f98f81c266207d09d2599139df371c3030a7b9
Pranav Singh <pranav@supportbee.com>
2017-09-12 14:57:41 +0530

    Configuring google creds for omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,6 +251,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :google_oauth2, "190695340637-cr7bskeeu590eds6l28ocjg8gm11dbh0.apps.googleusercontent.com", "w-CSRjdjZ5Jh73_QKhnxXtCl"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

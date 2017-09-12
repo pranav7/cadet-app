2b1236d6b9a946d35d6d11449ea79d83857e0f6c
Pranav Singh <pranav@supportbee.com>
2017-09-12 21:49:25 +0530

    Adds a basic dashboard view for an empty dashboard

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,13 +1,13 @@
 class Admin::PostsController < Admin::AdminController
   def show
     @posts = current_company.posts.order(created_at: :desc).includes(:comments)
-    @post = Post.find(params[:id]) || @posts.first
+    @post = Post.find(params[:id]) || @posts.first || nil
     @comment = @post.comments.new
     @comment.build_content
   end
 
   def index
     @posts = current_company.posts.order(created_at: :desc).includes(:comments)
-    redirect_to admin_post_path(@posts.first)
+    redirect_to admin_post_path(@posts.first) unless @posts.empty?
   end
 end

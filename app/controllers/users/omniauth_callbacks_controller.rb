de809d8807f77f093c6830a42fd263f46a8ef8c1
Pranav Singh <pranav@supportbee.com>
2017-09-12 22:03:34 +0530

    Updates user's company once the registration is complete

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,8 +12,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      if @user.companies.empty?
+        sign_in @user
+        redirect_to edit_user_registration_path
+      else
+        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+        set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+      end
     else
       session["devise.google_oauth2_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url

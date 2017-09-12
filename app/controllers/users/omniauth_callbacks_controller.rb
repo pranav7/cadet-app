0d73dbc98ef8fb79622442b2c337e335ad70a1ba
Pranav Singh <pranav@supportbee.com>
2017-09-12 16:57:31 +0530

    Removes binding

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      binding.pry
       sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else

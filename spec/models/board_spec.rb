4cd934b27151256ef77f214a7057ddb81f94e15c
Pranav Singh <pranav@supportbee.com>
2017-09-14 13:07:52 +0530

    Fixes board spec

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Board, type: :model do
   describe Board, "Associations" do
-    it { should belongs_to(:company) }
+    it { should belong_to(:company) }
     it { should have_many(:posts) }
   end
 end

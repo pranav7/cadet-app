7d32a9e32eb29aae9c1a4de0ff1012a030a9a40b
Pranav Singh <pranav@supportbee.com>
2017-09-20 12:01:25 +0530

    Adds date to admin list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,24 @@
 module ApplicationHelper
-  def render_time(created_at)
-    if created_at > 1.week.ago
-      "#{time_ago_in_words(created_at)} ago"
+  # User this helper to render time in the view
+  #   options :format [:short]
+  def render_time(created_at, options = {})
+    format = options.delete(:format)
+
+    case
+    when created_at > 1.week.ago
+      return "#{time_ago_in_words(created_at)} ago"
+    when created_at > 1.year.ago
+      if format && format == :short
+        return created_at.strftime("%e %b")
+      else
+        return created_at.strftime("%e %B, %Y")
+      end
     else
-      created_at.strftime("%e %B, %Y")
+      if format && format == :short
+        return created_at.strftime("%e %b, %y")
+      else
+        return created_at.strftime("%e %B, %Y")
+      end
     end
   end
 end

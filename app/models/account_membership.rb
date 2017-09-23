0556a0b0a9f98568778c0b7e52e4443546e6d503
Pranav Singh <pranav@supportbee.com>
2017-09-23 11:09:43 +0530

    Account memberships should be unique for user scoped to account

--- app/models/account_membership.rb
+++ app/models/account_membership.rb
@@ -2,6 +2,6 @@ class AccountMembership < ApplicationRecord
   belongs_to :user
   belongs_to :account
 
-  validates :user, presence: true
+  validates :user, presence: true, uniqueness: { scope: :account }
   validates :account, presence: true
 end

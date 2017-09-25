b57c08b51eb408badc5b0b5927607dfa743fd0e7
Pranav Singh <pranav@supportbee.com>
2017-09-25 13:00:50 +0530

    Adds a helper to build post link

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+  def build_post_link(post, options = {})
+    link_to = options.delete(:link_to) || :public
+
+    return admin_board_post_path(post.board, post) if link_to == :admin
+    board_post_path(post.board, post)
+  end
 end

af38c9adee29258e0df838eded9c636241b4b276
Pranav Singh <pranav@supportbee.com>
2017-10-09 17:11:55 +0530

    Rendering the correct link based on posts

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -3,7 +3,11 @@ module BoardsHelper
     to_admin = options.delete(:to_admin)
 
     if to_admin
-      admin_board_path(board)
+      if board.posts.blank?
+        admin_board_path(board)
+      else
+        admin_board_post_path(board, board.posts.first)
+      end
     else
       board_path(board)
     end

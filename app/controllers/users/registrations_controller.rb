33acdb0ce8c26dd1e4d229c45a7ea4ab8c9846fa
Pranav Singh <pranav@supportbee.com>
2017-10-13 17:20:49 +0530

    Permitting job title

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -61,13 +61,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:first_name,
-                                             :last_name,
+                                             :last_name
+                                             :job_title,
                                              memberships_attributes: [ company_attributes: [:name, :subdomain]]])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, memberships_attributes: [:role, company_attributes: [:name, :subdomain]]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :job_title, memberships_attributes: [:role, company_attributes: [:name, :subdomain]]])
   end
 
   def notify_slack  

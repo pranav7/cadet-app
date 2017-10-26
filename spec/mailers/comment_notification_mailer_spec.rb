d6b448df3f4279554d5cb2b282c55abbe29d1d90
Pranav Singh <pranav@supportbee.com>
2017-10-26 15:33:16 +0530

    Adds and updates comment notification mailer spec

--- spec/mailers/comment_notification_mailer_spec.rb
+++ spec/mailers/comment_notification_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe CommentNotificationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#new_comment" do
+    let(:user) { create :user }
+    let(:comment) { create :comment }
+    let(:mail) { CommentNotificationMailer.new_comment(user, comment) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("New Comment on #{comment.post.title}")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["hello@getcadet.com"])
+    end
+  end
 end

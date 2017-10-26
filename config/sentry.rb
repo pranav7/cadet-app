94267cb92ef897d2832efc1466f559a055e23eb1
Pranav Singh <pranav@supportbee.com>
2017-10-26 19:04:00 +0530

    Removing dev condition for now

---
+++ config/sentry.rb
@@ -0,0 +1,5 @@
+Raven.configure do |config|
+  config.dsn = Rails.application.secrets.sentry_dsn
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.environments = ['staging', 'production']
+end

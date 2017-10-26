c5102c26192b9cb08a9ec5fdd0c6e7d7b76c2f5a
Pranav Singh <pranav@supportbee.com>
2017-10-26 15:03:33 +0530

    Moves url logic to mailer

--- app/mailers/comment_notification_mailer.rb
+++ app/mailers/comment_notification_mailer.rb
@@ -4,6 +4,19 @@ class CommentNotificationMailer < ApplicationMailer
     @comment = comment
     @commenter = comment.created_by
     @post = comment.post
+    @company = @post.company
+
+    if @user.admin_of?(@company)
+      @board_url = admin_board_post_url(@post.board, @post, host: "#{@post.company.subdomain}.#{APP_CONFIG['base_domain']}")
+      @comment_url =
+        admin_board_post_url(@post.board, @post, host: "#{@post.company.subdomain}.#{APP_CONFIG['base_domain']}") + 
+        "#comment-#{@comment.id}"
+    else
+      @board_url = board_post_url(@post.board, @post, host: "#{@post.company.subdomain}.#{APP_CONFIG['base_domain']}")
+      @comment_url =
+        board_post_url(@post.board, @post, host: "#{@post.company.subdomain}.#{APP_CONFIG['base_domain']}") + 
+        "#comment-#{@comment.id}"
+    end
 
     mail(
       subject: "New Comment on #{@post.title}",

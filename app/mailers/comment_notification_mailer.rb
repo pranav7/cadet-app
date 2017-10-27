aee6da424d6ee7a75d70b4d807213dc668bd31c3
Pranav Singh <pranav@supportbee.com>
2017-10-27 17:26:48 +0530

    Fixing comment urls

--- app/mailers/comment_notification_mailer.rb
+++ app/mailers/comment_notification_mailer.rb
@@ -1,27 +1,28 @@
 class CommentNotificationMailer < ApplicationMailer
   def new_comment(comment, user)
     @user = user
-    @comment = comment
-    @commenter = comment.commenter
     @post = comment.post
     @company = @post.company
+    @comment = comment
+    @commenter = comment.commenter
     @host = @company.host
 
     if @user.admin_of?(@company)
       @board_url = admin_board_post_url(@post.board, @post, host: @host)
       @comment_url =
-        admin_board_post_url(@post.board, @post, host: @host)
+        admin_board_post_url(@post.board, @post, host: @host) +
         "#comment-#{@comment.id}"
     else
       @board_url = board_post_url(@post.board, @post, host: @host)
       @comment_url =
-        board_post_url(@post.board, @post, host: @host)
+        board_post_url(@post.board, @post, host: @host) +
         "#comment-#{@comment.id}"
     end
 
     mail(
       subject: "New Comment on #{@post.title}",
-      to: @user.email,
+      to: "#{@user.name} <#{@user.email}>",
+      form: from_address
     )
   end
 end

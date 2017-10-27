df8b3d572916e1553db90c9f49cad20a0deaa73e
Pranav Singh <pranav@supportbee.com>
2017-10-27 19:27:39 +0530

    Adds some specs and updates some text

--- spec/mailers/post_notification_mailer_spec.rb
+++ spec/mailers/post_notification_mailer_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 RSpec.describe PostNotificationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#new_post" do
+    let(:user) { create :user }
+    let(:requester) { create :customer }
+    let(:post) { create :post, user: requester }
+    let(:mail) { PostNotificationMailer.new_post(post, user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("New Post #{post.title} in #{post.board.name}")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["notifications@getcadet.com"])
+    end
+  end
 end

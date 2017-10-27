960a6c84332dc2a2365ae16d7d48f280416298e5
Pranav Singh <pranav@supportbee.com>
2017-10-27 17:45:41 +0530

    Removes this config moved to initializers

--- config/sentry.rb
+++
@@ -1,5 +0,0 @@
-Raven.configure do |config|
-  config.dsn = Rails.application.secrets.sentry_dsn
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments = ['staging', 'production']
-end

c390012325923665472335883033470a135fa16c
Pranav Singh <pranav@supportbee.com>
2017-10-27 17:03:26 +0530

    Adds methods to generate from address and signature

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'hello@getcadet.com'
+  default from: 'notifications@getcadet.com'
   layout 'mailer'
+
+  def from_address
+    "#{signature} <notifications@getcadet.com>"
+  end
+
+  def signature
+    @signature ||= @user.customer_of?(@company) ? "#{@company.name}" : "Team Cadet"
+  end
 end

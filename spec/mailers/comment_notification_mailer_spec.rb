6fc3efd443b6f3f540d5db59fd02add7c77b72ce
Pranav Singh <pranav@supportbee.com>
2017-11-02 16:41:45 +0530

    Fixing comment notification mailer spec

--- spec/mailers/comment_notification_mailer_spec.rb
+++ spec/mailers/comment_notification_mailer_spec.rb
@@ -3,13 +3,26 @@ require "rails_helper"
 RSpec.describe CommentNotificationMailer, type: :mailer do
   describe "#new_comment" do
     let(:user) { create :user }
-    let(:comment) { create :comment }
     let(:mail) { CommentNotificationMailer.new_comment(comment, user) }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("New Comment on #{comment.post.title}")
-      expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(["notifications@getcadet.com"])
+    context "new comment" do
+      let(:comment) { create :comment }
+
+      it "renders the correct headers" do
+        expect(mail.subject).to eq("New Reply: #{comment.post.title}")
+        expect(mail.to).to eq([user.email])
+        expect(mail.from).to eq(["notifications@getcadet.com"])
+      end
+    end
+
+    context "new note" do
+      let(:comment) { create :comment, private: true }
+
+      it "renders the correct headers" do
+        expect(mail.subject).to eq("New Note: #{comment.post.title}")
+        expect(mail.to).to eq([user.email])
+        expect(mail.from).to eq(["notifications@getcadet.com"])
+      end
     end
   end
 end

3240b744ecc5a14a118b682650db66f46a49ee34
Pranav Singh <pranav@supportbee.com>
2017-12-06 17:03:02 +0530

    Adds invitation controller code

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -11,7 +11,32 @@ class Users::InvitationsController < Devise::InvitationsController
 
   # POST /resource/invitation
   def create
-    super
+    begin
+      validate_role
+      validate_membership
+
+      self.resource = find_or_invite_resource do |user|
+        membership = user.memberships.build(company: current_company, role: params[:role].downcase)
+        user.skip_invitation = true if params[:send_invitation] != "true"
+      end
+
+      resource_invited = resource.errors.empty?
+
+      yield resource if block_given?
+
+      if resource_invited
+        if is_flashing_format? && self.resource.invitation_sent_at
+          set_flash_message :notice, :send_instructions, email: self.resource.email
+        end
+
+        respond_with resource, location: admin_users_path
+      else
+        respond_with_navigational(resource) { redirect_to admin_users_path }
+      end
+    rescue => e
+      flash[:error] = e.message
+      redirect_to admin_users_path
+    end
   end
 
   def edit
@@ -21,16 +46,19 @@ class Users::InvitationsController < Devise::InvitationsController
   def update
     super
   end
+  
+  protected
 
-  private
-
-  def invite_resource
-    super do |user|
-      validate_role
-      user.memberships.build(company: current_company, role: params[:role].downcase)
-      user.skip_invitation = true if params[:send_invitation] != "true"
+  def find_or_invite_resource(&block)
+    if user = User.find_by_email(invite_params[:email])
+      user.memberships.create(company: current_company, role: params[:role].downcase)
+      user
+    else
+      resource_class.invite!(invite_params, current_inviter, &block)
     end
   end
+
+  private
 
   def after_invite_path_for(current_inviter)
     admin_users_path
@@ -48,6 +76,16 @@ class Users::InvitationsController < Devise::InvitationsController
 
   def validate_role
     return if Membership.roles.keys.include?(params[:role].downcase)
-    raise BadRequest.new("The role provided is invalid")
+    raise "The role provided is invalid"
+  end
+
+  def validate_membership
+    user = User.find_by_email(invite_params[:email])
+    return unless user
+
+    membership = current_company.memberships.where(user: user).first 
+    if membership
+      raise "Oops! A user with this email already exists."
+    end
   end
 end

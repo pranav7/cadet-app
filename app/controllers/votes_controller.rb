af0abd7e2f911356e7d2bd48d7307c55ddd01df4
Pranav Singh <pranav@supportbee.com>
2017-12-08 17:26:28 +0530

    Updates controller endpoint to pass user id for creating vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,14 +3,20 @@ class VotesController < ApplicationController
   before_action :load_post
 
   def create
-    @post.votes.create(user: current_user)
+    if params[:user_id]
+      user = User.find params[:user_id]
+    else
+      user = current_user
+    end
+    @post.votes.create(user: user)
     
-    unless current_user.part_of?(current_company)
-      current_user.companies << current_company
+    unless user.part_of?(current_company)
+      user.companies << current_company
     end
 
     respond_to do |format|
       format.html do
+        flash[:success] = "Vote Added!" if params[:user_id]
         redirect_back fallback_location: board_post_path(@board, @post)
       end
     end

2ccf8b2290618a1937913a0d7bcbeb86c2e879a1
Pranav Singh <pranav@supportbee.com>
2017-12-15 01:51:49 +0530

    Fixing new post content in email

--- spec/mailers/previews/post_notification_mailer_preview.rb
+++ spec/mailers/previews/post_notification_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/post_notification_mailer
 class PostNotificationMailerPreview < ActionMailer::Preview
   def new_post
-    PostNotificationMailer.new_post(Post.first, User.first)
+    PostNotificationMailer.new_post(Post.last, User.last)
   end
 
   def upvote

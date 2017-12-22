24fb5cd2f955681fe5e0de27bff2d7045908a34f
Pranav Singh <pranav@supportbee.com>
2017-12-22 16:46:48 +0530

    Sets up a basic button which updates count based on state

--- app/assets/javascripts/components/upvote_button.es6.jsx
+++ app/assets/javascripts/components/upvote_button.es6.jsx
@@ -1,9 +1,34 @@
 class UpvoteButton extends React.Component {
-  render () {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      upvoted: this.props.upvoted,
+      voteCount: this.props.voteCount
+    };
+
+    this.clickListener = this.clickListener.bind(this);
+ }
+
+  clickListener() {
+    if (this.state.upvoted) {
+      this.setState({
+        voteCount: this.state.voteCount - 1,
+        upvoted: false
+      });
+    } else {
+      this.setState({
+        voteCount: this.state.voteCount + 1,
+        upvoted: true
+      })
+    }
+  }
+
+  render() {
     return (
-      <a className="vote-button large" data-method="post" href="/feature-requests/posts/this-is-an-example-post/votes">
-        <span className="text">Upvote</span>
-        <span className="vote-count">1</span>
+      <a className={this.state.upvoted ? 'upvoted vote-button' : 'vote-button'} onClick={this.clickListener}>
+        <span className="text">{this.state.upvoted ? 'Upvoted' : 'Upvote'}</span>
+        <span className="vote-count">{this.state.voteCount}</span>
       </a>
     );
   }

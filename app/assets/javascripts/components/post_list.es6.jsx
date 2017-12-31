23096e8d04ab684e57dccb208447039f85586741
Pranav Singh <pranav@supportbee.com>
2017-12-31 15:41:53 +0530

    Adds post list component

--- app/assets/javascripts/components/post_list.es6.jsx
+++ app/assets/javascripts/components/post_list.es6.jsx
@@ -1,7 +1,41 @@
 class PostList extends React.Component {
-  render () {
-    return <div />;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      boardId: this.props.boardId,
+      posts: []
+    };
+
+    this.getPosts = this.getPosts.bind(this);
+    this.getPosts();
+  }
+
+  getPosts() {
+    let that = this;
+    axios({
+      method: "GET",
+      url: `/${this.state.boardId}/posts`,
+      headers: {
+        'Content-Type': "application/json",
+        'Accept': "application/json",
+        'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
+      }
+    })
+    .then(response => {
+      this.setState({
+        posts: response.data.posts
+      });
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.posts.map((post) =>
+          <PostListItem boardId={this.state.boardId} post={post} />
+        )}
+      </div>
+    );
   }
 }
-
-

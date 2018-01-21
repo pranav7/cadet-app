3f4a01a6d69f4b6e24723d34b9b549fec93c34dd
Pranav Singh <pranav@supportbee.com>
2018-01-22 02:54:31 +0530

    Rendering parsed markdown

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,24 @@
+require 'github/markup'
+
 class Content < ApplicationRecord
   belongs_to :parent, polymorphic: true, optional: true
 
   validates :body, presence: true
+
+  def parsed_markdown
+    context = {
+      asset_root: "/images/",
+      gfm: true
+    }
+
+    pipeline = HTML::Pipeline.new [
+      HTML::Pipeline::MarkdownFilter,
+      HTML::Pipeline::SanitizationFilter,
+      HTML::Pipeline::RougeFilter,
+      HTML::Pipeline::EmojiFilter,
+      HTML::Pipeline::AutolinkFilter,
+    ], context
+
+    pipeline.call(body)[:output].to_s
+  end
 end

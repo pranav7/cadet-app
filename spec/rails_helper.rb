6da761c9d0f21e0ecc366dca71104b8198e76949
Pranav Singh <pranav@supportbee.com>
2018-02-06 17:44:39 +0530

    Capybara config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,11 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require'capybara/rspec'
+require 'capybara/rails'
+require 'capybara/rspec'
+
+Capybara.server = :puma # Until your setup is working
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -63,5 +67,9 @@ RSpec.configure do |config|
       with.test_framework :rspec
       with.library :rails
     end
+  end
+
+  Capybara.configure do |config|
+    config.always_include_port = true
   end
 end

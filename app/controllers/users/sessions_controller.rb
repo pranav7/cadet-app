828a90b77dc6cdb0e898c86af116ee380d42b128
Pranav Singh <pranav@supportbee.com>
2018-03-08 16:50:27 +0530

    Always remember session

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -15,7 +15,6 @@ class Users::SessionsController < Devise::SessionsController
       session[:user_return_to] = URI(request.referer).path 
       session[:subdomain] = current_company.subdomain
     end
-    params[:user][:remember_me] = true
 
     super
   end

3f347afd5d4bcad4cda0f8883255a1e7a53819bb
Pranav Singh <pranav@supportbee.com>
2018-03-14 20:19:34 +0530

    Set Current.user if user is signed in

--- app/controllers/concerns/set_current_request_details.rb
+++ app/controllers/concerns/set_current_request_details.rb
@@ -6,6 +6,7 @@ module SetCurrentRequestDetails
       Current.request_id = request.uuid
       Current.user_agent = request.user_agent
       Current.ip_address = request.ip
+      Current.user = current_user if user_signed_in?
     end
   end
 end

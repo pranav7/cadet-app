5ccd27d4e463605ee5eec55143f2fe366a8d4941
Pranav Singh <pranav@supportbee.com>
2018-03-14 19:47:06 +0530

    Refactoring more code from ApplicationController to Concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,15 @@
 class ApplicationController < ActionController::Base
   include SetCurrentCompany
   include ValidateCompanyExpiry
+  include PrepareExceptionNotifier
+  include AuthorizeAdminAccess
 
   before_action :drop_naked_ip_requests
   protect_from_forgery preprend: true, with: :exception
-  before_action :prepare_exception_notifier
 
   protected
     def not_found
       raise ActionController::RoutingError.new('Not Found')
-    end
-
-    def authorize_admin_access!
-      authenticate_user!
-      not_found unless current_user.admin_of?(current_company)
     end
 
   private
@@ -30,16 +26,6 @@ class ApplicationController < ActionController::Base
 
     def after_sign_out_path_for(resource_or_scope)
       request.referrer || stored_location_for(resource_or_scope) || super
-    end
-
-    def prepare_exception_notifier
-      exception_data = {
-        url: request.url,
-        ip: request.ip
-      }
-      exception_data[:current_user] = current_user.serializable_hash if user_signed_in?
-
-      request.env["exception_notifier.exception_data"] = exception_data
     end
 
     def drop_naked_ip_requests

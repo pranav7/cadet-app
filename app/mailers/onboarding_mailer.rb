d047206ef7e3b2a944ee345e42fb0cb7c5d4d000
Pranav Singh <pranav@supportbee.com>
2018-04-25 13:04:51 +0530

    Add mailers for trial reminder and expiry

--- app/mailers/onboarding_mailer.rb
+++ app/mailers/onboarding_mailer.rb
@@ -8,4 +8,26 @@ class OnboardingMailer < ApplicationMailer
       from: "Pranav from Cadet <pranav@getcadet.com>"
     )
   end
+
+  def trial_reminder(user, company)
+    @user = user
+    @company = company
+
+    mail(
+      subject: "Checking-in for your Cadet Trial",
+      to: @user.formatted_address,
+      from: "Pranav from Cadet <pranav@getcadet.com>"
+    )
+  end
+
+  def trial_expired(user, company)
+    @user = user
+    @company = company
+
+    mail(
+      subject: "Your Cadet Trial & Subscription",
+      to: @user.formatted_address,
+      from: "Pranav from Cadet <pranav@getcadet.com>"
+    )
+  end
 end
--- spec/mailers/previews/onboarding_mailer_preview.rb
+++ spec/mailers/previews/onboarding_mailer_preview.rb
@@ -3,4 +3,12 @@ class OnboardingMailerPreview < ActionMailer::Preview
   def welcome
     OnboardingMailer.welcome(User.last)
   end
+
+  def trial_reminder
+    OnboardingMailer.trial_reminder(User.last, User.last.companies.first)
+  end
+
+  def trial_expired
+    OnboardingMailer.trial_expired(User.last, User.last.companies.first)
+  end
 end

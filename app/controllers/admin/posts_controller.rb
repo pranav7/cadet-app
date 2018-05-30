e5093432c1588dd2db3e9722ef4d83ba9260b47a
Pranav Singh <pranav@bizongo.in>
2018-05-30 18:22:03 +0530

    Use the .search method on Post

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,9 +3,7 @@ class Admin::PostsController < Admin::AdminController
     @board = current_company.boards.friendly.find(params[:board_id])
 
     if params[:search] && params[:search] != ""
-      # @posts = @board.posts.search_by_title(params[:search].downcase)
-      posts = Post.arel_table
-      @posts = @board.posts.where(posts[:title].matches("%#{params[:search].downcase}%"))
+      @posts = @board.posts.search(term: params[:search])
     else
       @posts = @board.posts.sorted(sort_method: params[:sort_by])
         .reverse_chronologically

d93ccf54d6629b061d81e5d2e44d6232a1f86cb8
Pranav Singh <pranav@bizongo.in>
2018-06-09 15:15:51 +0530

    Define comment target

--- app/javascript/packs/controllers/comments_controller.js
+++ app/javascript/packs/controllers/comments_controller.js
@@ -1,11 +1,14 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+  static targets = ["source"]
+
   connect() {
     console.log("I got connected! I am Comments Controller.")
   }
 
   editClickHandler() {
-    console.log("Edit Clicked!")
+    $(`#edit-comment-${this.sourceTarget.dataset.value}-modal`).
+      modal("show")
   }
 }

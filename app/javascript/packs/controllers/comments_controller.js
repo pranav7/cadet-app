c867b738788e1ccb5bad88424e0fde6338efea21
Pranav Singh <pranav@bizongo.in>
2018-06-20 20:07:12 +0530

    Remove connect method

--- app/javascript/packs/controllers/comments_controller.js
+++ app/javascript/packs/controllers/comments_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["source"]
 
-  connect() {
-    console.log("I got connected! I am Comments Controller.")
-  }
-
   editClickHandler() {
     $(`#edit-comment-${this.sourceTarget.dataset.value}-modal`).
       modal("show")

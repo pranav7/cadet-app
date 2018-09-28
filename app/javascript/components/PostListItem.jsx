49a0206975cd5054979bb117b2501228703eda36
Pranav Singh <pranav@bizongo.in>
2018-09-28 18:34:12 +0530

    Make PostListItem a stateless component

--- app/javascript/components/PostListItem.jsx
+++ app/javascript/components/PostListItem.jsx
@@ -2,58 +2,45 @@ import React from "react";
 import UpvoteButton from "./UpvoteButton";
 import Cookies from "js-cookie";
 
-class PostListItem extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      boardId: this.props.boardId,
-      post: this.props.post
-    };
-
-    this.handlePostItemClick = this.handlePostItemClick.bind(this);
-  }
-
-  handlePostItemClick() {
+const PostListItem = ({post, boardId}) => {
+  function handlePostItemClick() {
     Cookies.set("scrollPos", $(window).scrollTop())
   }
 
-  render() {
-    return (
-      <div className="post-list-item">
-        <div className="post-body">
-          <a  className="post-link"
-              href={this.state.post.url}
-              onClick={this.handlePostItemClick}>
-            <div className="title-text float left">
-              {this.state.post.title}
-            </div>
-            <div className="post-summary">
-              {this.state.post.summary}
-            </div>
-          </a>
-          <div className="comment-count soft float right">
-            <i className="comment outline icon" />
-            {this.state.post.comments_count}
+  return (
+    <div className="post-list-item">
+      <div className="post-body">
+        <a  className="post-link"
+            href={post.url}
+            onClick={handlePostItemClick}>
+          <div className="title-text float left">
+            {post.title}
           </div>
-        </div>
-        <div className="post-info soft">
-          <UpvoteButton voteCount={this.state.post.votes_count}
-            upvoted={this.state.post.upvoted}
-            boardId={this.state.boardId}
-            postId={this.state.post.id} />
-          <div className="meta">
-            <strong className={`status ${this.state.post.status}`}>
-              #{this.state.post.status}
-            </strong>
-            <span className="dates">
-              {this.state.post.created_at} by {this.state.post.created_by}
-            </span>
+          <div className="post-summary">
+            {post.summary}
           </div>
+        </a>
+        <div className="comment-count soft float right">
+          <i className="comment outline icon" />
+          {post.comments_count}
         </div>
       </div>
-    );
-  }
+      <div className="post-info soft">
+        <UpvoteButton voteCount={post.votes_count}
+          upvoted={post.upvoted}
+          boardId={boardId}
+          postId={post.id} />
+        <div className="meta">
+          <strong className={`status ${post.status}`}>
+            #{post.status}
+          </strong>
+          <span className="dates">
+            {post.created_at} by {post.created_by}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PostListItem;

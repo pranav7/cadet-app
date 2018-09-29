38a70235d94527523608584a70ad20cd4c246259
Pranav Singh <pranav@bizongo.in>
2018-09-29 13:17:57 +0530

    Define an AdminApp with Routes

---
+++ app/javascript/components/AdminApp.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import { BrowserRouter, Route } from 'react-router-dom'
+import PostView from "./admin/PostView"
+
+class AdminApp extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="AdminApp">
+          <Route  path="/admin/:board_id/:post_id"
+                  render={(props) => <PostView {...props} boardId={this.props.boardId} />} />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default AdminApp

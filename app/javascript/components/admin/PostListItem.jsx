043a6b06778e97340169f871ea97f9b98e81b175
Pranav Singh <pranav@bizongo.in>
2018-10-01 23:35:31 +0530

    Use class based component

--- app/javascript/components/admin/PostListItem.jsx
+++ app/javascript/components/admin/PostListItem.jsx
@@ -1,30 +1,36 @@
 import React from "react";
-import UpvoteButton from "../UpvoteButton";
 import { Link } from 'react-router-dom'
 
-const PostListItem = ({post, boardId}) => {
-  return (
-    <Link className="admin-post-list-item" to={`/admin/${boardId}/${post.slug}`}>
-      <div className="post-title">{post.title}</div>
-      <div className="post-summary c-soft">{post.summary}</div>
-      <div className="post-meta c-soft">
-        <span>{post.created_at}</span>
-        <span>&nbsp;</span>
-        <span className={`small status ${post.status}`}>
-          {`#${post.status}`}
-        </span>
-        <span>&nbsp;</span>
-        <span className="ui rounded tiny label soft">
-          {post.votes_count} Upvote
-        </span>
-        <span>&nbsp;</span>
-        <span className="ui rounded tiny label soft">
-          <i className="comment outline icon" />
-          {post.comments_count}
-        </span>
-      </div>
-    </Link>
-  );
+class PostListItem extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Link className="admin-post-list-item"
+            to={`/admin/${this.props.boardId}/posts/${this.props.post.slug}`} >
+        <div className="post-title">{this.props.post.title}</div>
+        <div className="post-summary c-soft">{this.props.post.summary}</div>
+        <div className="post-meta c-soft">
+          <span>{this.props.post.created_at}</span>
+          <span>&nbsp;</span>
+          <span className={`small status ${this.props.post.status}`}>
+            {`#${this.props.post.status}`}
+          </span>
+          <span>&nbsp;</span>
+          <span className="ui rounded tiny label soft">
+            {this.props.post.votes_count} Upvote
+          </span>
+          <span>&nbsp;</span>
+          <span className="ui rounded tiny label soft">
+            <i className="comment outline icon" />
+            {this.props.post.comments_count}
+          </span>
+        </div>
+      </Link>
+    )
+  }
 }
 
 export default PostListItem

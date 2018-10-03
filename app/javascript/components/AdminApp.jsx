595bda7ab7b96ca4e09cb79ee5a6931dd7d2c22e
Pranav Singh <pranav@bizongo.in>
2018-10-03 12:34:04 +0530

    Make AdminApp the root component and have different routes for each component

--- app/javascript/components/AdminApp.jsx
+++ app/javascript/components/AdminApp.jsx
@@ -1,16 +1,46 @@
 import React, { Component } from "react"
-import { BrowserRouter, Route } from 'react-router-dom'
-import PostView from "./admin/PostView"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import PostDetails from "./admin/PostDetails";
+import PostList from "./admin/PostList";
 
 class AdminApp extends Component {
   render() {
+    const routes = [
+      {
+        path: "/admin/:boardId/posts/:postId",
+        exact: true,
+        sidebar: PostList,
+        main: PostDetails
+      }
+    ];
+
     return (
-      <BrowserRouter>
-        <div className="AdminApp">
-          <Route  path="/admin/:boardId/posts/:postId"
-                  render={(props) => <PostView {...props} boardId={this.props.boardId} />} />
+      <Router>
+        <div className="AdminApp c-dashboard-grid">
+          <div className="c-left-pane">
+            <div className="list-action-bar"></div>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={route.sidebar}
+              />
+            ))}
+          </div>
+          <div className="c-main-pane">
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={route.main}
+              />
+            ))}
+          </div>
+          <div className="c-right-pane"></div>
         </div>
-      </BrowserRouter>
+      </Router>
     )
   }
 }

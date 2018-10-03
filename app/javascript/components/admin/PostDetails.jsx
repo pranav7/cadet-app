4cf65f72a464d535ab9d81ebe0f812bed21327ef
Pranav Singh <pranav@bizongo.in>
2018-10-03 17:37:20 +0530

    Refactor comments into a method, use renderHTML to render html

--- app/javascript/components/admin/PostDetails.jsx
+++ app/javascript/components/admin/PostDetails.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import renderHTML from 'react-render-html'
 import Posts from '../../wrappers/Posts'
 import UpvoteButton from '../UpvoteButton';
 
@@ -11,6 +12,7 @@ class PostDetails extends Component {
     }
 
     this.getPost = this.getPost.bind(this)
+    this.renderComment = this.renderComment.bind(this)
   }
 
   componentDidMount() {
@@ -30,26 +32,98 @@ class PostDetails extends Component {
       })
   }
 
+  renderComment(comment) {
+    if(comment.private) {
+      return (
+        <div className="note" key={comment.id}>
+          <div className="user">{comment.commenter.name}</div>
+          <div className="content box">
+            {renderHTML(comment.content.body)}
+            <div className="meta-info soft">
+              {comment.created_at}
+            </div>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="comment-container" key={comment.id}>
+          <div className="comment box">
+            <div className="user">
+              <div className="details">
+                <span className="name">{comment.commenter.name}</span>
+              </div>
+            </div>
+            <div className="content">
+              {renderHTML(comment.content.body)}
+            </div>
+            <div className="meta-info soft">
+              {comment.created_at}
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+
   render() {
     if(this.state.post != null) {
       return (
-        <div className="post-header">
-          <div className="header">
-            <h1 className="post-title">{this.state.post.title}</h1>
-            <div className="post-header-info">
-              <strong className={`status ${this.state.post.status}`}>
-                #{this.state.post.status}
-              </strong>
+        <div className="c-main-pane">
+          <div className="top-action-bar">
+            <div className="item">
+              <a href="" id="edit-post-btn">
+                <i className="edit outline large icon"></i>
+              </a>
             </div>
-           </div>
-           <div className="votes">
-             <UpvoteButton
-                voteCount={this.state.post.votes_count}
-                upvoted={this.state.post.upvoted}
-                boardId={this.props.boardId}
-                postId={this.props.postId}
-             />
-           </div>
+            <div className="item">
+              <a href="" id="delete-post-btn">
+                <i className="trash alternate outline large icon"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="post-header">
+            <div className="header">
+              <h1 className="post-title">{this.state.post.title}</h1>
+              <div className="post-header-info">
+                <strong className={`status ${this.state.post.status}`}>
+                  #{this.state.post.status}
+                </strong>
+              </div>
+            </div>
+            <div className="votes">
+              <UpvoteButton
+                  voteCount={this.state.post.votes_count}
+                  upvoted={this.state.post.upvoted}
+                  boardId={this.props.boardId}
+                  postId={this.props.postId}
+              />
+            </div>
+          </div>
+
+          <div className="post-content vertically padded">
+            {this.renderComment({
+              content: this.state.post.content,
+              created_at: this.state.post.created_at,
+              id: this.state.post.id,
+              commenter: this.state.post.requester
+            })}
+          </div>
+
+          <div className="post-activity">
+            <div className="activity-header">
+              <div className="text">
+                <i className="comments outline icon" />
+                Conversation
+              </div>
+            </div>
+            {this.state.post.comments.map((comment) =>
+              <div className="comment-container">
+                {this.renderComment(comment)}
+              </div>
+            )}
+          </div>
         </div>
       )
     } else {

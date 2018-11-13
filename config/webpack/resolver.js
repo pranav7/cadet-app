eaf6b8edc9dfe589f23aae85d81108a83211c254
Pranav Singh <pranav@bizongo.in>
2018-11-13 17:28:39 +0530

    Add config Webpack Resolve

---
+++ config/webpack/resolver.js
@@ -0,0 +1,11 @@
+module.exports = {
+  resolve: {
+    alias: {
+      API: "api/",
+      Components: 'components/',
+      AdminComponents: 'components/admin',
+      Services: 'services/',
+      Reducers: 'reducers'
+    }
+  }
+}

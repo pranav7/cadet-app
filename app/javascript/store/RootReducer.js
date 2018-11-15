75aa1bdaeedcd2de194330772e1c7b01c2d20231
Pranav Singh <pranav@bizongo.in>
2018-11-15 20:05:38 +0530

    Add reducers for new actions

--- app/javascript/store/RootReducer.js
+++ app/javascript/store/RootReducer.js
@@ -1,6 +1,54 @@
-import { combineReducers } from 'redux';
-import PostReducer from 'Modules/Posts/Reducer';
+/* {
+ *    isFetching: <true/false>,
+ *    didInvalidate: <true/false>,
+ *    lastUpdated: <Timestamp>,
+ *    selectedPost: {
+ *      id: 10,
+ *      title: "",
+ *      comments: [],
+ *      voters: []
+ *    },
+ *    posts: []
+ * }
+ */
 
-export default combineReducers({
-  selectedPost: PostReducer
-});
+// import { combineReducers } from 'redux';
+// import PostReducer from 'Modules/Posts/Reducer';
+import {
+  UPVOTED,
+  REQUEST_POST,
+  RECEIVE_POST
+} from 'Modules/Posts/Actions';
+
+const initialState = {
+  isFetching: false,
+  didInvalidate: false,
+  selectedPost: null
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case UPVOTED:
+      let newState = Object.assign({}, state)
+      newState.selectedPost.voters.push(action.voter)
+      return newState;
+    case REQUEST_POST:
+      return Object.assign({}, state, {
+        isFetching: true,
+        didInvalidate: false
+      })
+    case RECEIVE_POST:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        selectedPost: action.post
+      });
+    default:
+      return state;
+  }
+}
+
+// export default combineReducers({
+//   selectedPost: PostReducer
+// });
+

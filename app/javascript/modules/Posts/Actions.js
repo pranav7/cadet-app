3cd9f06072a65bd8ad04379ece4579818f25ce6f
Pranav Singh <pranav@bizongo.in>
2018-11-15 20:03:20 +0530

    Add action and actions creators

--- app/javascript/modules/Posts/Actions.js
+++ app/javascript/modules/Posts/Actions.js
@@ -1,3 +1,31 @@
+import Posts from 'API/Posts';
+
 export const UPVOTED = 'UPVOTED';
 export const DOWNVOTED = 'DOWNVOTED';
-export const POST_FETCHED = 'POST_FETCHED';
+
+export const REQUEST_POST = 'REQUEST_POST';
+function requestPost() {
+  return {
+    type: REQUEST_POST
+  }
+}
+
+export const RECEIVE_POST = 'RECEIVE_POST';
+function receivePost(post) {
+  return {
+    type: RECEIVE_POST,
+    post
+  }
+}
+
+export function fetchPost(boardId, postId) {
+  return function(dispatch) {
+    dispatch(requestPost())
+
+    const postsAPI = new Posts(boardId, { postId })
+    postsAPI.getOne()
+      .then(response => {
+        dispatch(receivePost(response.post));
+      })
+  }
+}

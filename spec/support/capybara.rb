526b763ba6b5f358b679c644a63f11a80c8688ed
Pranav Singh <pranav@bizongo.in>
2018-12-09 15:05:14 +0530

    Remove unwanted capybara config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,15 +4,4 @@ RSpec.configure do |config|
   Capybara.default_driver = :rack_test
   Capybara.javascript_driver = JS_DRIVER
   Capybara.default_max_wait_time = 2
-
-  Capybara.register_driver :selenium do |app|
-    if ENV['SELENIUM_DRIVER_URL'].present?
-      Capybara::Selenium::Driver.new(
-        app,
-        browser: :remote,
-        url: ENV.fetch('SELENIUM_DRIVER_URL'),
-        desired_capabilities: :chrome
-      )
-    end
-  end
 end

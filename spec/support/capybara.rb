82ff7b085c7759de1f04065e38fe77491c4c9ffd
Pranav Singh <pranav@bizongo.in>
2018-12-09 15:02:42 +0530

    Minor fixes

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,10 +1,10 @@
 JS_DRIVER = :selenium_chrome_headless
 
-Capybara.default_driver = :rack_test
-Capybara.javascript_driver = JS_DRIVER
-Capybara.default_max_wait_time = 2
-
 RSpec.configure do |config|
+  Capybara.default_driver = :rack_test
+  Capybara.javascript_driver = JS_DRIVER
+  Capybara.default_max_wait_time = 2
+
   Capybara.register_driver :selenium do |app|
     if ENV['SELENIUM_DRIVER_URL'].present?
       Capybara::Selenium::Driver.new(
@@ -13,12 +13,6 @@ RSpec.configure do |config|
         url: ENV.fetch('SELENIUM_DRIVER_URL'),
         desired_capabilities: :chrome
       )
-    else
-      Capybara::Selenium::Driver.new(app, browser: :chrome)
     end
-  end
-
-  config.after(:each) do
-    Capybara.use_default_driver
   end
 end

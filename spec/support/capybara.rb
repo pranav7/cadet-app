7a3dc96e0414323928357d19fe9780d9f0f34eae
Pranav Singh <pranav@bizongo.in>
2018-12-09 14:47:06 +0530

    Add Selenium related driver url

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,6 +5,19 @@ Capybara.javascript_driver = JS_DRIVER
 Capybara.default_max_wait_time = 2
 
 RSpec.configure do |config|
+  Capybara.register_driver :selenium do |app|
+    if ENV['SELENIUM_DRIVER_URL'].present?
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :remote,
+        url: ENV.fetch('SELENIUM_DRIVER_URL'),
+        desired_capabilities: :chrome
+      )
+    else
+      Capybara::Selenium::Driver.new(app, browser: :chrome)
+    end
+  end
+
   config.after(:each) do
     Capybara.use_default_driver
   end

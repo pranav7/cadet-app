bc2b40b58647ccaaebe3a93043c327507ff3182f
Pranav Singh <pranav@bizongo.in>
2019-03-10 22:28:14 +0000

    Handle changes to dropdown and save changes to backend

--- app/javascript/components/admin/containers/StatusDropdown.js
+++ app/javascript/components/admin/containers/StatusDropdown.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Dropdown } from 'semantic-ui-react';
 
+import Posts from 'API/Posts';
+
 const statusOptions = [
+  {
+    text: '#open',
+    value: 'open'
+  },
   {
     text: '#planned',
     value: 'planned'
@@ -9,14 +15,53 @@ const statusOptions = [
   {
     text: '#developing',
     value: 'developing'
+  },
+  {
+    text: '#released',
+    value: 'released'
+  },
+  {
+    text: '#closed',
+    value: 'closed'
   }
 ];
 
-const StatusDropdown = () => (
-  <span>
-    Status{' '}
-    <Dropdown inline options={statusOptions} />
-  </span>
-)
+class StatusDropdown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e, { name, value }) {
+    const postsApi = new Posts(this.props.boardId, { postId: this.props.postId });
+    const data = {
+      post: {
+        status: value
+      }
+    };
+
+    postsApi.update(data)
+      .then(response => {
+        console.log("updated", response);
+      })
+      .catch(response => {
+        console.log("failed", response);
+      });
+  }
+
+  render() {
+    return(
+      <span>
+        Status{' '}
+        <Dropdown
+          inline
+          options={statusOptions}
+          onChange={this.handleChange}
+        />
+      </span>
+    );
+  }
+}
 
 export default StatusDropdown;

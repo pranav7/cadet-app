8dfe1e78b27460a9927357c2f8b1f0e7c2bd66a8
Pranav Singh <pranav.singh@intercom.io>
2019-06-18 11:25:02 +0100

    Convert to class component

--- app/javascript/components/admin/containers/CreatePostModal.js
+++ app/javascript/components/admin/containers/CreatePostModal.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Modal, Form, Input, TextArea } from 'semantic-ui-react';
 
 import MarkdownStyling from 'Common/MarkdownStyling';
 
-const CreatePostModal = (props) => (
-  <Modal
-    trigger={props.children}
-    centered={false}
-    size="tiny"
-  >
-    <Modal.Header>Create a Post</Modal.Header>
-    <Modal.Content>
-      <Modal.Description>
-        <Form>
-          <Form.Field>
-            <Form.Input placeholder='Title' />
-          </Form.Field>
-          <Form.Field>
-            <Form.TextArea placeholder='Description' rows={13} />
-          </Form.Field>
+class CreatePostModal extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-          <Button type='submit'>Create</Button>
-          <MarkdownStyling />
-        </Form>
-      </Modal.Description>
-    </Modal.Content>
-  </Modal>
-);
+  render() {
+    return (
+      <Modal
+        trigger={this.props.children}
+        centered={false}
+        size="tiny"
+      >
+        <Modal.Header>Create a Post</Modal.Header>
+        <Modal.Content>
+          <Modal.Description>
+            <Form>
+              <Form.Field>
+                <Form.Input placeholder='Title' />
+              </Form.Field>
+              <Form.Field>
+                <Form.TextArea placeholder='Description' rows={13} />
+              </Form.Field>
+
+              <Button type='submit'>Create</Button>
+              <MarkdownStyling />
+            </Form>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
+    );
+  }
+}
 
 export default CreatePostModal;

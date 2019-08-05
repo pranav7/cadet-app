a348a80067313d1c72293756c2d5a023aeb51d18
Pranav Singh <pranav.singh@intercom.io>
2019-08-06 00:14:27 +0100

    Add search to admin post list

--- app/javascript/components/admin/PostList.js
+++ app/javascript/components/admin/PostList.js
@@ -1,32 +1,25 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { Container, Icon } from 'semantic-ui-react';
+import { connect } from "react-redux";
+import { Container, Icon } from "semantic-ui-react";
 
 import PostListItem from "./PostListItem";
 import Cookies from "js-cookie";
 import _ from "underscore";
-import { fetchPosts } from 'Modules/Posts/Actions';
-import CreatePostModal from 'AdminContainers/CreatePostModal';
+import { fetchPosts } from "Modules/Posts/Actions";
+import CreatePostModal from "AdminContainers/CreatePostModal";
 
 class PostList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      boardId: this.props.match.params.boardId
+      boardId: this.props.match.params.boardId,
+      searchTerm: "",
+      loading: false
     };
-
-    this.init = this.init.bind(this);
-    this.getPosts = this.getPosts.bind(this);
-    this.renderPostList = this.renderPostList.bind(this);
-    this.handlePostItemClick = this.handlePostItemClick.bind(this);
   }
 
   componentDidMount() {
-    this.init();
-  }
-
-  init() {
     if (_.isUndefined(Cookies.get("currentSortOrder"))) {
       this.getPosts();
     } else {
@@ -35,47 +28,66 @@ class PostList extends React.Component {
     }
   }
 
-  getPosts(params = {}) {
+  getPosts = (params = {}) => {
     this.props.dispatch(fetchPosts(this.state.boardId, params));
-  }
+  };
 
-  handlePostItemClick(slug) {
+  handleSearchInput = event => {
+    if (event.target.value == "") {
+      this.setState(
+        { searchTerm: event.target.value, loading: false },
+        this.search
+      );
+    } else {
+      this.setState(
+        { searchTerm: event.target.value, loading: true },
+        this.search
+      );
+    }
+  };
+
+  search = () => {
+    this.getPosts({ search: this.state.searchTerm });
+  };
+
+  handlePostItemClick = slug => {
     this.setState({ currentSelected: slug });
-  }
+  };
 
-  renderPostList() {
+  renderPostList = () => {
     if (this.props.posts.length > 0) {
       return (
         <div className="post-list-item-container">
-          {this.props.posts.map((post) =>
+          {this.props.posts.map(post => (
             <React.Fragment key={post.id}>
-              <PostListItem boardId={this.state.boardId}
+              <PostListItem
+                boardId={this.state.boardId}
                 selected={this.state.currentSelected == post.slug}
                 post={post}
                 onPostItemClick={this.handlePostItemClick}
-                {...this.props} />
+                {...this.props}
+              />
             </React.Fragment>
-          )}
+          ))}
         </div>
       );
-    } else if (!this.props.noPosts &&
-      this.props.isFetchingPosts) {
-      return(
+    } else if (!this.props.noPosts && this.props.isFetchingPosts) {
+      return (
         <Container textAlign="center" className="top padded">
-          <Icon loading name='circle notch' size="large" color="grey" />
+          <Icon loading name="circle notch" size="large" color="grey" />
         </Container>
       );
     } else {
-      return(
-        <div className="post-list-item">
+      return (
+        <div className="t__soft u__pt__x2 u__pl__x4">
           There are no matching posts
         </div>
       );
     }
-  }
+  };
 
   render() {
-    return(
+    return (
       <div className="c-left-pane">
         <div className="list-action-bar">
           <div className="container-one">
@@ -94,19 +106,22 @@ class PostList extends React.Component {
           <div className="container-two">
             <div className="ui icon fluid input field">
               <i className="search icon"></i>
-              <input name="search" placeholder="Search Posts"></input>
+              <input
+                type="text"
+                placeholder="Search"
+                value={this.state.searchTerm}
+                onChange={this.handleSearchInput}
+              />
             </div>
           </div>
         </div>
-        <div className="post-list-container">
-          {this.renderPostList()}
-        </div>
+        <div className="post-list-container">{this.renderPostList()}</div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     isFetchingPosts: state.isFetchingPosts,
     noPosts: state.noPosts,

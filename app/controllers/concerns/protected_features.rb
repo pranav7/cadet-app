66e74fbeb72b1f9b5caf44a31bf97cafa700074a
Pranav Singh <hello@pranavsingh.me>
2020-02-22 12:19:46 +0000

    Add protected feature concern

---
+++ app/controllers/concerns/protected_features.rb
@@ -0,0 +1,14 @@
+module ProtectedFeatures
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :can_access_feature?
+
+    def can_access_feature?
+      # return true if Rails.env.development?
+      return false unless current_company.cadet_app?
+      return false unless user_signed_in? && current_user.admin_of?(current_company)
+      true
+    end
+  end
+end

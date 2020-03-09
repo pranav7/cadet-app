1a412a89e222cd6159b6eae6d775265cdade2d31
srikar0896 <srikar.0896@gmail.com>
2020-03-09 20:36:59 +0530

    added to app config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -302,7 +302,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, APP_CONFIG['google_oauth2']['client_id'], APP_CONFIG['google_oauth2']['client_secret']
 
-  config.omniauth :intercom, '459610f5-95ab-419d-bbe7-f22f104a46bd', 'bbe9e1e2-9d5e-47f0-a449-4b1c43ff8561'
+  config.omniauth :intercom, APP_CONFIG['intercom']['client_id'], APP_CONFIG['intercom']['client_secret']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

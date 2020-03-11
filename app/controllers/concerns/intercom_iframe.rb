14559667d540a2c40cefc75e309d756a51e2635a
srikar0896 <srikar.0896@gmail.com>
2020-03-11 21:42:49 +0530

    hide header

--- app/controllers/concerns/intercom_iframe.rb
+++ app/controllers/concerns/intercom_iframe.rb
@@ -4,10 +4,6 @@ module IntercomIframe
   included do
     skip_after_action :intercom_rails_auto_include
 
-    before_action do
-      session[:hide_public_header] = true if params[:intercom_iframe]
-    end
-
     after_action do
       response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM https://intercom-sheets.com"
     end

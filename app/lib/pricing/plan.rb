94a18125ee9d2227f84bb427177ea94344eabcd6
Pranav Singh <hello@pranavsingh.me>
2020-03-17 12:12:14 +0000

    Checkpoint

--- app/lib/pricing/plan.rb
+++ app/lib/pricing/plan.rb
@@ -2,8 +2,9 @@ module Pricing
   class Plan
     PLANS_CONFIG = "#{Rails.root}/config/pricing.yml".freeze
     ACTIVE_VERSION = "v1.1".freeze
+    TRIAL_PERIOD = 14
 
-    attr_reader :base_price, :per_user_price, :per_user_modulus, :paddle_product_id
+    attr_reader :base_price, :per_user_price, :per_user_modulus, :paddle_product_id, :version
 
     def initialize(version: nil)
       @version = version || ACTIVE_VERSION
--- spec/lib/pricing/plan_spec.rb
+++ spec/lib/pricing/plan_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 describe ::Pricing::Plan do
+  let(:version) { nil }
+  subject { described_class.new(version: version) }
+
+  context "no version is specified" do
+    it "returns the active version by default" do
+      expect(subject.version).to eq(described_class::ACTIVE_VERSION)
+    end
+  end
+
+  context "version is specified" do
+    let(:version) { "v1" }
+
+    it "returns the correct version" do
+      expect(subject.version).to eq(version)
+    end
+  end
 end

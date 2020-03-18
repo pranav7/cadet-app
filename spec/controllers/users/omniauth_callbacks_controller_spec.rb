b70775c24a3a56ff5061d2f0ae3452ef2669b42f
Pranav Singh <pranav.singh@intercom.io>
2020-03-18 17:58:22 +0000

    Fix specs

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe OmniauthCallbacksController, type: :controller do
   before do
     request.host = "#{company.subdomain}.example.com"
     sign_in user
-
-    set_omniauth_intercom
-    get user_intercom_omniauth_authorize_path
   end
 
   describe "Triggers intercom callback" do
-
     it "saves access token successfully" do
-      expect(company.company_setting.intercom_access_token).to  eq("dG9rOmNdrWt0ZjtgzzE0MDdfNGM5YVe4MzsmXzFmOGd2MDhiMfJmYTrxOtA=")
+      mock_omniauth_intercom
+      get user_intercom_omniauth_authorize_path
+
+      expect(company.company_setting.intercom_access_token)
+        .to eq("dG9rOmNdrWt0ZjtgzzE0MDdfNGM5YVe4MzsmXzFmOGd2MDhiMfJmYTrxOtA=")
     end
 
   end

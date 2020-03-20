5812392809a66d0323e11ae4b54c3311e1d4255b
srikar0896 <srikar.0896@gmail.com>
2020-03-20 22:40:15 +0530

    added size "tiny" to labels

--- app/javascript/components/Account.js
+++ app/javascript/components/Account.js
@@ -6,11 +6,11 @@ const Account = ({ id, name, votes, mrr }) => {
     <div className="u__p__x2" style={{ display: 'flex' }}>
       <span class="u__pr__x2">{name}</span>
       <Label.Group circular>
-        <Label>
+        <Label size="tiny">
           <i className="caret up icon"></i>
           {votes}
         </Label>
-        <Label as='a'>${mrr}/mo</Label>
+        <Label size="tiny">${mrr}/mo</Label>
       </Label.Group>
     </div>
   );

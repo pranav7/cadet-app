f29794cffc009b508f04abb4795539cc947ae0a0
Pranav Singh <hello@pranavsingh.me>
2020-04-03 19:29:11 +0100

    eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    semi: 2
+  }
+}

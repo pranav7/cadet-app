47a3bc55f65e853701c6a1ac1cbda59886ff7462
srikar0896 <srikar.0896@gmail.com>
2020-04-06 21:30:10 +0530

    restrict from adding duplicate voter

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,24 +5,59 @@ class VotesController < ApplicationController
   def create
     if params[:user_id]
       user = User.find params[:user_id]
-      @post.votes.create(user: user, added_by: current_user)
+      if not @post.voters.where(id: user.id).first
+        @post.votes.create(user: user, added_by: current_user)
+        respond_to do |format|
+          format.html do
+            flash[:success] = "Vote for #{user.name} was added" if params[:user_id]
+            redirect_back fallback_location: board_post_path(@board, @post)
+          end
+    
+          format.json do
+            head :created
+          end
+        end
+      else
+        respond_to do |format|
+          format.html do
+            flash[:success] = "Vote for #{user.name} was already added" if params[:user_id]
+            redirect_back fallback_location: board_post_path(@board, @post)
+          end
+    
+          format.json do
+            head :ok
+          end
+        end
+      end
     else
       user = current_user
-      @post.votes.create(user: user)
+      if not @post.voters.where(id: user.id).first
+        @post.votes.create(user: user)
+        respond_to do |format|
+          format.html do
+            flash[:success] = "Vote for #{user.name} was added" if params[:user_id]
+            redirect_back fallback_location: board_post_path(@board, @post)
+          end
+    
+          format.json do
+            head :created
+          end
+        end
+      else
+        respond_to do |format|
+          format.html do
+            flash[:success] = "Vote for #{user.name} was already added" if params[:user_id]
+            redirect_back fallback_location: board_post_path(@board, @post)
+          end
+    
+          format.json do
+            head :ok
+          end
+        end
+      end
     end
 
     user.companies << current_company unless user.part_of?(current_company)
-
-    respond_to do |format|
-      format.html do
-        flash[:success] = "Vote for #{user.name} was added" if params[:user_id]
-        redirect_back fallback_location: board_post_path(@board, @post)
-      end
-
-      format.json do
-        head :created
-      end
-    end
   end
 
   def destroy

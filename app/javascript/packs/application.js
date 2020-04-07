fd3fc09c3bd1c7472c738a1c44a70f00192efc4e
Pranav Singh <hello@pranavsingh.me>
2020-04-07 19:46:46 +0100

    Update lint

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,19 +7,22 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-import * as Sentry from '@sentry/browser';
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+import * as Sentry from "@sentry/browser";
 
-if (process.env.NODE_ENV === 'production') {
-  Sentry.init({dsn: "https://2234c89fb9f34a1aaae5a8910141202e@o374501.ingest.sentry.io/5192625"});
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn:
+      "https://2234c89fb9f34a1aaae5a8910141202e@o374501.ingest.sentry.io/5192625"
+  });
 }
 
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
 
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true)
-var ReactRailsUJS = require("react_ujs")
-ReactRailsUJS.useContext(componentRequireContext)
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);

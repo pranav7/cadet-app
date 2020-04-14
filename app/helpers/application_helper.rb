5041798e042c9ed683e76e6049af40942849dab3
Pranav Singh <hello@pranavsingh.me>
2020-04-14 10:42:47 +0100

    Update how we render time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,14 +7,10 @@ module ApplicationHelper
     case
     when created_at > 1.week.ago
       return "#{time_ago_in_words(created_at)} ago"
-    when created_at > 1.year.ago
-      return created_at.strftime("%e %b") if format && format == :short
-
-      created_at.strftime("%e %B, %Y")
+    when created_at > 60.days.ago
+      created_at.strftime("%e %b")
     else
-      return created_at.strftime("%e %b, %y") if format && format == :short
-
-      created_at.strftime("%e %B, %Y")
+      created_at.strftime("%e %b, %Y")
     end
   end
 

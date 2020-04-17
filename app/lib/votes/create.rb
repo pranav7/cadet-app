f8a7deb63ab9a902970736c1bf775d1d01a2aa2f
Pranav Singh <hello@pranavsingh.me>
2020-04-17 18:20:23 +0100

    Refactor Votes::Create

--- app/lib/votes/create.rb
+++ app/lib/votes/create.rb
@@ -2,8 +2,10 @@ module Votes
   class Create
     attr_reader :post, :voter
 
-    def self.run(post:, voter:)
-      new(post: post, voter: voter).run
+    def self.run!(post:, voter:)
+      service = new(post: post, voter: voter)
+      service.validate!
+      service.run!
     end
 
     def initialize(post:, voter:)
@@ -11,31 +13,32 @@ module Votes
       @voter = voter
     end
 
-    def valid?
-      return false if voter.voted?(post)
-      return false unless user_has_permission?
-      true
+    def validate!
+      validate_user_has_permission
+      validate_user_has_not_voted
     end
 
-    def run
-      create_vote if valid?
-    end
-
-    private
-
-    def create_vote
+    def run!
       post.votes.create(user: voter, added_by: added_by)
       voter.companies << Current.company unless voter.part_of?(Current.company)
     end
+
+    private
 
     def added_by
       return if Current.user == voter
       Current.user
     end
 
-    def user_has_permission?
-      return true if Current.user == voter
-      Current.user.admin_of?(Current.company)
+    def validate_user_has_permission
+      return if Current.user == voter
+      return if Current.user.admin_of?(Current.company)
+      raise Errors::AdminLacksPermission
+    end
+
+    def validate_user_has_not_voted
+      return unless voter.voted?(post)
+      raise Errors::ServiceValidationError, "User already upvoted the post"
     end
   end
 end

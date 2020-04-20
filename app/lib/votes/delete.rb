ca46ef81bc8720c6173468245f25aba06c5c0d6a
Pranav <pranav.singh@intercom.io>
2020-04-20 18:56:08 +0100

    Add Votes Delete Service (#83)

---
+++ app/lib/votes/delete.rb
@@ -0,0 +1,13 @@
+module Votes
+  class Delete < Base
+    attr_reader :post, :voter
+
+    def validate!
+      validate_user_has_permission
+    end
+
+    def run!
+      Vote.where(post: post, user: voter).first&.destroy!
+    end
+  end
+end

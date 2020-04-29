d2250e178ffbedf8edda5c6d2f7da7a1ded2413c
srikar0896 <srikar.0896@gmail.com>
2020-04-29 22:40:39 +0530

    add index on company_id and post_id

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2020_04_16_160500) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_activity_logs_on_post_id"
+    t.index ["post_id", "company_id"], name: "index_activity_logs_on_post_id_and_company_id"
   end
 
   create_table "boards", force: :cascade do |t|

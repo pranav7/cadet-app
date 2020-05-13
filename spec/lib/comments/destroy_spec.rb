c26a81e9927747e04fbcdf0bb2947996273031fd
srikar0896 <srikar.0896@gmail.com>
2020-05-13 23:29:24 +0530

    added specs for destroy

--- spec/lib/comments/destroy_spec.rb
+++ spec/lib/comments/destroy_spec.rb
@@ -4,12 +4,12 @@ describe Comments::Destroy do
   let(:company) { create :company }
   let(:board) { create :board, company: company }
   let(:user) { create :user }
+  let(:user2) { create :user }
   let(:admin) { create :admin, company: company }
   let(:post) { create :post, board: board }
   let(:comment) { create :comment, post: post, commenter: user }
   let(:comment_created_event) { create :comment_created_event, post: post, company: company, comment: comment }
-  let(:activity_log) { create :activity_log, post: post, company: company }
-
+  let!(:activity_log) { create :activity_log, :comment_created_event, event_id: comment_created_event.id, company: comment_created_event.company, post: comment_created_event.post }
   subject { described_class.run!(comment: comment) }
 
   before do
@@ -18,11 +18,22 @@ describe Comments::Destroy do
   end
 
   it "deletes the comment" do
-    binding.pry
-    expect { subject }.to change(Comment, :count).by(1)
-    # expect(comment.commenter).to eq(user)
-    # expect(comment.content.body).to eq("Test Body")
-    # expect(comment.post).to eq(post)
-    # expect(comment.private).to eq(is_private)
+    subject
+
+    expect(Comment.count).to eq(0)
+    expect(CommentCreatedEvent.count).to eq(0)
+    expect(ActivityLog.count).to eq(0)
+  end
+
+  context "validations" do
+    before do
+      Current.user = user2
+    end
+
+    it "throws Insufficient permissions error" do
+      subject
+
+      expect { subject }.to raise_error(Errors::AdminLacksPermission)
+    end
   end
 end

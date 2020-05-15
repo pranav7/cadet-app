5b46333cfa4517fee0e9d01a46e30d88a8ea73cd
Pranav Singh <hello@pranavsingh.me>
2020-05-15 18:48:19 +0100

    Add activity log to the show page

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -2,6 +2,8 @@ class ActivityLog < ApplicationRecord
   belongs_to :company
   belongs_to :post
 
+  scope :public_activity, -> { where(visibility: Constants::Visibility::PUBLIC) }
+
   def event
     Constants::EventTypes::CLASSES[event_type].find(event_id)
   end

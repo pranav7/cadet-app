bde2e8c406ddee94e3d3ee30c166f0423097ec02
Pranav Singh <hello@pranavsingh.me>
2020-05-17 15:01:53 +0100

    Rename scope

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -2,7 +2,7 @@ class ActivityLog < ApplicationRecord
   belongs_to :company
   belongs_to :post
 
-  scope :public_activity, -> { where(visibility: Constants::Visibility::PUBLIC) }
+  scope :publicly_visible, -> { where(visibility: Constants::Visibility::PUBLIC) }
 
   def event
     Constants::EventTypes::CLASSES[event_type].find(event_id)

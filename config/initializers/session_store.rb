349e2a03421e1675f4388b6bcdacf20107bbf0d7
Pranav Singh <hello@pranavsingh.me>
2020-10-29 19:14:38 +0000

    Use domain name in session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_cadet_app_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: "_cadet_app_session_#{Rails.env}", domain: :all

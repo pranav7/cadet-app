cb60ae15777ee07f4cf46551ecca2b8d64290d85
Pranav Singh <hello@pranavsingh.me>
2020-10-30 13:55:19 +0000

    Restore all domain cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_cadet_app_session_#{Rails.env}", domain: APP_CONFIG[:base_domain]
+Rails.application.config.session_store :cookie_store, key: "_cadet_app_session_#{Rails.env}", domain: :all

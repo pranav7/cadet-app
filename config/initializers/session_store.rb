08d1782030c6f91b5cfb6fc4a971306c5a18c9fc
Pranav Singh <hello@pranavsingh.me>
2021-01-10 00:53:19 +0000

    Fix session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_cadet_app_session_#{Rails.env}", domain: :all, same_site: :none, secure: true, tld_length: 2
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: "_cadet_app_session", domain: "getcadet.com", same_site: :none, secure: true, tld_length: 2
+else
+  Rails.application.config.session_store :cookie_store, key: "_cadet_app_session", domain: "lvh.me"
+end

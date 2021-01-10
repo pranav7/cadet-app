5e3fa0121897ad453572ffde7d098f7347bfd82f
Pranav Singh <hello@pranavsingh.me>
2021-01-10 20:35:06 +0000

    Fix production session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: "_cadet_app_session", domain: "getcadet.com", same_site: :none, secure: true, tld_length: 2
+  Rails.application.config.session_store :cookie_store, key: "_cadet_app_session", domain: :all, same_site: :none, secure: true, tld_length: 2
 else
   Rails.application.config.session_store :cookie_store, key: "_cadet_app_session", domain: "lvh.me"
 end

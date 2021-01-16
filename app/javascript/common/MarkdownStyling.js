4d0a4f3400e0691d86a24d64c2a573598fc1c1e9
Pranav Singh <hello@pranavsingh.me>
2021-01-16 23:16:57 +0000

    Various UI changes

--- app/javascript/common/MarkdownStyling.js
+++ app/javascript/common/MarkdownStyling.js
@@ -3,7 +3,7 @@ import { GoMarkdown } from 'react-icons/go';
 
 const MarkdownStyling = () => (
   <a
-    className="styling-with-markdown inline-flex flex-row items-center gap-1"
+    className="styling-with-markdown inline-flex flex-row items-center justify-items-end"
     href="https://guides.github.com/features/mastering-markdown/"
     target="_blank"
   >

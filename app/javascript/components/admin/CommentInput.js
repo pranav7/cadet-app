f12d8202288d764c7a65fb3224747f502f94218a
Pranav Singh <hello@pranavsingh.me>
2021-02-20 21:19:56 +0000

    Style fixes caused by tailwind

--- app/javascript/components/admin/CommentInput.js
+++ app/javascript/components/admin/CommentInput.js
@@ -1,27 +1,24 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Grid, Form, TextArea, Tab, Button, Popup } from "semantic-ui-react";
-import { osName } from "react-device-detect";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Grid, Form, TextArea, Tab, Button, Popup } from 'semantic-ui-react';
+import { osName } from 'react-device-detect';
 
-import Posts from "API/Posts";
-import { fetchPost } from "Modules/Posts/Actions";
-import MarkdownStyling from "Common/MarkdownStyling";
-import { MentionsInput, Mention } from "react-mentions";
-import Users from "API/Users";
-import {
-  CommentTextAreaWithMentionStyles,
-  MentionStyles
-} from "Common/MentionsStyling";
-import MentionSuggestion from "Components/MentionSuggestion";
+import Posts from 'API/Posts';
+import { fetchPost } from 'Modules/Posts/Actions';
+import MarkdownStyling from 'Common/MarkdownStyling';
+import { MentionsInput, Mention } from 'react-mentions';
+import Users from 'API/Users';
+import { CommentTextAreaWithMentionStyles, MentionStyles } from 'Common/MentionsStyling';
+import MentionSuggestion from 'Components/MentionSuggestion';
 
 class CreateComment extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      comment: "",
-      note: "",
-      users: []
+      comment: '',
+      note: '',
+      users: [],
     };
 
     this.handleNoteChange = this.handleNoteChange.bind(this);
@@ -34,23 +31,23 @@ class CreateComment extends Component {
   componentDidMount() {
     const usersApi = new Users();
 
-    usersApi.get().then(response => {
+    usersApi.get().then((response) => {
       this.setState({
-        users: response.data.users.map(user => ({
+        users: response.data.users.map((user) => ({
           id: user.username,
-          display: user.name
-        }))
+          display: user.name,
+        })),
       });
     });
     if (this.props.value && this.props.value.comment) {
       this.setState({
-        comment: this.props.value.comment
+        comment: this.props.value.comment,
       });
     }
 
     if (this.props.value && this.props.value.note) {
       this.setState({
-        note: this.props.value.note
+        note: this.props.value.note,
       });
     }
   }
@@ -72,61 +69,61 @@ class CreateComment extends Component {
   submitComment() {
     if (this.props.value) {
       // https://github.com/signavio/react-mentions/issues/78
-      this.props.onSubmit(this.state.comment.replace(/@@/g, "@"));
+      this.props.onSubmit(this.state.comment.replace(/@@/g, '@'));
       return;
     }
 
     const postsApi = new Posts(this.props.boardId, {
-      postId: this.props.postId
+      postId: this.props.postId,
     });
     const data = {
       comment: {
         content_attributes: {
-          body: this.state.comment.replace(/@@/g, "@")
-        }
-      }
+          body: this.state.comment.replace(/@@/g, '@'),
+        },
+      },
     };
 
     postsApi
       .comment(data)
-      .then(response => {
+      .then((response) => {
         this.props.dispatch(fetchPost(this.props.boardId, this.props.postId));
-        this.setState({ comment: "" });
+        this.setState({ comment: '' });
       })
-      .catch(response => {
+      .catch((response) => {
         // TODO: Add Notification Toast
-        console.log("Error Creating Comment", response);
+        console.log('Error Creating Comment', response);
       });
   }
 
   submitNote() {
     if (this.props.value) {
       // https://github.com/signavio/react-mentions/issues/78
-      this.props.onSubmit(this.state.note.replace(/@@/g, "@"));
+      this.props.onSubmit(this.state.note.replace(/@@/g, '@'));
       return;
     }
 
     const postsApi = new Posts(this.props.boardId, {
-      postId: this.props.postId
+      postId: this.props.postId,
     });
     const data = {
       comment: {
         private: true,
         content_attributes: {
-          body: this.state.note.replace(/@@/g, "@")
-        }
-      }
+          body: this.state.note.replace(/@@/g, '@'),
+        },
+      },
     };
 
     postsApi
       .comment(data)
-      .then(response => {
+      .then((response) => {
         this.props.dispatch(fetchPost(this.props.boardId, this.props.postId));
-        this.setState({ note: "" });
+        this.setState({ note: '' });
       })
-      .catch(response => {
+      .catch((response) => {
         // TODO: Add Notification Toast
-        console.log("Error Creating Comment", response);
+        console.log('Error Creating Comment', response);
       });
   }
 
@@ -137,7 +134,7 @@ class CreateComment extends Component {
           <MentionsInput
             value={this.state.comment}
             onChange={this.handleCommentChange}
-            onKeyDown={event => {
+            onKeyDown={(event) => {
               this.handleCmdEnter(event, this.submitComment);
             }}
             id="newComment"
@@ -150,18 +147,12 @@ class CreateComment extends Component {
               trigger="@"
               data={this.state.users}
               style={MentionStyles}
-              displayTransform={username => `@${username}`}
+              displayTransform={(username) => `@${username}`}
               appendSpaceOnAdd
               // https://github.com/signavio/react-mentions/issues/78
               regex={/@@([\w_-]+)/}
               markup="@@__id__"
-              renderSuggestion={(
-                suggestion,
-                query,
-                highlightedDisplay,
-                index,
-                focused
-              ) => (
+              renderSuggestion={(suggestion, query, highlightedDisplay, index, focused) => (
                 <MentionSuggestion
                   suggestion={suggestion}
                   query={query}
@@ -172,32 +163,20 @@ class CreateComment extends Component {
               )}
             />
           </MentionsInput>
-          <Grid verticalAlign="middle">
-            <Grid.Row>
-              <Grid.Column width={6}>
-                <Popup
-                  content={this.cmdOrCtrl()}
-                  size="mini"
-                  position="bottom center"
-                  trigger={
-                    <Button
-                      primary
-                      type="submit"
-                      size="tiny"
-                      disabled={this.state.comment == ""}
-                    >
-                      Reply
-                    </Button>
-                  }
-                  inverted
-                />
-              </Grid.Column>
-
-              <Grid.Column textAlign="right" width={10}>
-                <MarkdownStyling />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+          <div className="flex flex-row justify-between">
+            <Popup
+              content={this.cmdOrCtrl()}
+              size="mini"
+              position="bottom center"
+              trigger={
+                <Button primary type="submit" size="tiny" disabled={this.state.comment == ''}>
+                  Reply
+                </Button>
+              }
+              inverted
+            />
+            <MarkdownStyling />
+          </div>
         </Form>
       </Tab.Pane>
     );
@@ -212,7 +191,7 @@ class CreateComment extends Component {
           <MentionsInput
             value={this.state.note}
             onChange={this.handleNoteChange}
-            onKeyDown={event => {
+            onKeyDown={(event) => {
               this.handleCmdEnter(event, this.submitNote);
             }}
             id="newNote"
@@ -225,18 +204,12 @@ class CreateComment extends Component {
               trigger="@"
               data={this.state.users}
               style={MentionStyles}
-              displayTransform={username => `@${username}`}
+              displayTransform={(username) => `@${username}`}
               // https://github.com/signavio/react-mentions/issues/78
               appendSpaceOnAdd
               regex={/@@([\w_-]+)/}
               markup="@@__id__"
-              renderSuggestion={(
-                suggestion,
-                query,
-                highlightedDisplay,
-                index,
-                focused
-              ) => (
+              renderSuggestion={(suggestion, query, highlightedDisplay, index, focused) => (
                 <MentionSuggestion
                   suggestion={suggestion}
                   query={query}
@@ -247,43 +220,30 @@ class CreateComment extends Component {
               )}
             />
           </MentionsInput>
-
-          <Grid verticalAlign="middle">
-            <Grid.Row>
-              <Grid.Column width={6}>
-                <Popup
-                  content={this.cmdOrCtrl()}
-                  size="mini"
-                  position="bottom center"
-                  trigger={
-                    <Button
-                      primary
-                      type="submit"
-                      size="tiny"
-                      disabled={this.state.note == ""}
-                    >
-                      Add Note
-                    </Button>
-                  }
-                  inverted
-                />
-              </Grid.Column>
-
-              <Grid.Column textAlign="right" width={10}>
-                <MarkdownStyling />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+          <div className="flex flex-row justify-between">
+            <Popup
+              content={this.cmdOrCtrl()}
+              size="mini"
+              position="bottom center"
+              trigger={
+                <Button primary type="submit" size="tiny" disabled={this.state.note == ''}>
+                  Add Note
+                </Button>
+              }
+              inverted
+            />
+            <MarkdownStyling />
+          </div>
         </Form>
       </Tab.Pane>
     );
   }
 
   cmdOrCtrl() {
-    if (osName == "Mac OS") {
-      return "⌘+Enter";
+    if (osName == 'Mac OS') {
+      return '⌘+Enter';
     } else {
-      return "Ctrl+Enter";
+      return 'Ctrl+Enter';
     }
   }
 
@@ -297,8 +257,8 @@ class CreateComment extends Component {
     }
 
     const panes = [
-      { menuItem: "Reply", render: () => this.renderComment() },
-      { menuItem: "Note", render: () => this.renderNote() }
+      { menuItem: 'Reply', render: () => this.renderComment() },
+      { menuItem: 'Note', render: () => this.renderNote() },
     ];
 
     return <Tab menu={{ secondary: true, pointing: true }} panes={panes} />;
